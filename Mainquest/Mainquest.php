2f25c5efad44641b3adcbc35785fa08521a677b4
Matheus Musashi <126472450+MatheusMusashiTanaka@users.noreply.github.com>
2024-09-26 16:50:14 -0300

    Add files via upload

---
+++ Mainquest/Mainquest.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Pixelify+Sans:wght@400..700&display=swap" rel="stylesheet">
+</head>
+<body>
+<div class="BFD">
+    <div class="display">
+        <div class="status">
+            <div><a> Life:  12/23. </a></div>
+            <div><a> Armor: 6. </a></div>
+            <div><a> Equipment:  </a></div>
+            <div><a> Inventory[  </a></div>
+            <div><a>      Potion.  </a></div>
+            <div><a> ]  </a></div>
+        </div>
+        <div class="centering">
+            <a>roberto</a>    
+        <img class="enemy" src="https://preview.redd.it/the-pixel-token-for-ur-enemies-35-gold-and-it-turns-your-v0-sta1sq6s8bfd1.png?width=3000&format=png&auto=webp&s=fb8b7e9d2f2cb865cb40f6fbc81b3f5449c6c01a"></div>
+        <div class="actions"></div>
+
+    </div>
+
+
+</div>
+
+
+
+<?php
+    
+
+?>
+</body>
+</html>
